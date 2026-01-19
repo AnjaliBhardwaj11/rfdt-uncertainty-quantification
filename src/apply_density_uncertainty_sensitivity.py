Fix calculation of E_lo and E_hi in sensitivity function

src/apply_density_uncertainty_sensitivity.py
@@ -54,8 +54,8 @@ s = S / S_med
 
 q_total = q_beam * inflation * s
 
-E_lo = E_hat * torch.exp(mu - q_total)
-E_hi = E_hat * torch.exp(mu + q_total)
+E_lo = E_hat * torch.exp(- q_total)
+E_hi = E_hat * torch.exp(+ q_total)
 
 torch.save(
     {